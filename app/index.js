c87d2b3da67412ae02863888439de9be463729a1
Amr Desouky <desoukya@gmail.com>
2016-07-12 09:04:20 -0400

    Lesson 3: React Router

--- app/index.js
+++ app/index.js
@@ -1,69 +1,9 @@
-var USER_DATA = {
-  name: 'Amr Desouky',
-  username: 'desoukya',
-  image: 'http://2.gravatar.com/avatar/ce791f717724172cd44a46878325a23e'
-}
-
 var React      = require('react');
 var ReactDOM   = require('react-dom');
-
-var ProfilePic = React.createClass({
-  render() {
-    return (
-      <img src={this.props.imageUrl} style={{height:100, weidth:100}} />
-    );
-  }
-});
-
-var Link = React.createClass({
-  changeURL() {
-    window.location.replace(this.props.href)
-  },
-  render() {
-    return (
-      <span style={{color: 'blue', 'cursor': 'pointer'}}
-            onClick={this.changeURL}>
-        {this.props.children}
-      </span>
-    );
-  }
-})
-
-var ProfileLink = React.createClass({
-  render() {
-    return (
-      <div>
-        <Link href={'https://www.github.com/' + this.props.username} >
-          {this.props.username}
-        </Link>
-      </div>
-    );
-  }
-});
-
-var ProfileName = React.createClass({
-  render() {
-    return (
-      <div> {this.props.name} </div>
-    );
-  }
-});
-
-// parent component
-var Avatar = React.createClass({
-  render() {
-    return (
-      <div>
-        <ProfilePic imageUrl={this.props.user.image} />
-        <ProfileName name={this.props.user.name} />
-        <ProfileLink username={this.props.user.username} />
-      </div>
-    );
-  }
-});
+var routes     = require('./config/routes');
 
 //Attach Component to DOM
-ReactDOM.render(
-  <Avatar user={USER_DATA} />,
+ReactDOM.render (
+  routes,
   document.getElementById('app')
-)
+);
